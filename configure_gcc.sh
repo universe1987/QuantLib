c9240cda668ec265a8ede550fcbfb88cc1560b87
Peter Caspers <pcaspers1973@gmail.com>
2015-07-17 13:01:47 +0200

    suppress deprecated declarations warning

--- configure_gcc.sh
+++ configure_gcc.sh
@@ -1,2 +1,2 @@
 # configure gcc, no open mp support
-./configure --with-boost-include=/home/peter/boost_1_57_0 --with-boost-lib=/home/peter/boost_1_57_0/stage/lib --enable-error-lines CXXFLAGS="-m64 -O3 -g -Wall -Wno-unused-local-typedefs -Wno-unknown-pragmas -std=c++11 -I home/peter/adolc_base/include" LDFLAGS="-L/home/peter/adolc_base/lib64" LIBS="-ladolc"
+./configure --with-boost-include=/home/peter/boost_1_57_0 --with-boost-lib=/home/peter/boost_1_57_0/stage/lib --enable-error-lines CXXFLAGS="-m64 -O3 -g -Wall -Wno-unused-local-typedefs -Wno-unknown-pragmas -Wno-deprecated-declarations -std=c++11 -I/home/peter/adolc_base/include" LDFLAGS="-L/home/peter/adolc_base/lib64" LIBS="-ladolc"
