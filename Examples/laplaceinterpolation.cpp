38470743d67169c28404311265e56abb86cd0970
Peter Caspers <pcaspers1973@gmail.com>
2015-11-19 10:02:45 +0100

    update example

--- Examples/laplaceinterpolation.cpp
+++ Examples/laplaceinterpolation.cpp
@@ -6,45 +6,57 @@ using namespace QuantLib;
 
 int main() {
 
-    const int N = 25;
-    const double delPerc = 0.9;
+    int N = 100;
+    double delPerc = 0.9;
+    char *arg1 = getenv("DELPERC");
+    if (arg1 != NULL)
+        delPerc = atof(arg1);
+    char *arg2 = getenv("N");
+    if (arg2 != NULL)
+        N = atoi(arg2);
 
-    Matrix sample(N,N);
+    Matrix sample(N, N);
 
-    for(Size i=0;i<N;++i) {
-        for(Size j=0;j<N;++j) {
-            sample[i][j] = sin((double)i/N*4.0)*cos((double)j/N*4.0);
+    for (Size i = 0; i < N; ++i) {
+        for (Size j = 0; j < N; ++j) {
+            sample(i, j) =
+                sin((double)(i + 1) / N * 4.0) * cos((double)(j + 1) / N * 4.0);
         }
     }
 
-    // std::cout << "Original Matrix:" << std::endl;
+    // std::cout << "Original Matrix:" << sample.size1() << "x" <<
+    // sample.size2() << std::endl;
     // std::cout << sample << std::endl;
 
     MersenneTwisterUniformRng mt(42);
 
-    Matrix sample2(sample), del(N,N);
+    Matrix sample2(sample), del(N, N);
 
-    for(Size l=0;l<(N*N*delPerc);++l) {
+    for (Size l = 0; l < (N * N * delPerc); ++l) {
         Size i = static_cast<Size>(mt.nextReal() * N);
         Size j = static_cast<Size>(mt.nextReal() * N);
-        sample2[i][j] = Null<Real>();
-        del[i][j] = 1;
+        if (sample2[i][j] != Null<Real>()) {
+            sample2[i][j] = Null<Real>();
+            del[i][j] = 1;
+        } else {
+            --l;
+        }
     }
 
     // std::cout << "Randomly erased points:" << std::endl;
     // std::cout << sample << std::endl;
 
     // std::cout << "Reconstructed matrix:" << std::endl;
-    laplaceInterpolation(sample2,1E-6);
+    laplaceInterpolation(sample2, 1E-6);
     // std::cout << sample << std::endl;
 
-    for(Size i=0;i<N;++i) {
-        for(Size j=0;j<N;++j) {
-            std::cout << i << " " << j << " " << sample[i][j] << " " << sample2[i][j] << " " << del[i][j] << std::endl;
+    for (Size i = 0; i < N; ++i) {
+        for (Size j = 0; j < N; ++j) {
+            std::cout << i << " " << j << " " << sample[i][j] << " "
+                      << sample2[i][j] << " " << del[i][j] << std::endl;
         }
         std::cout << std::endl;
     }
 
     return 0;
-
 }
--- Examples/laplaceinterpolation.gp
+++ Examples/laplaceinterpolation.gp
@@ -1,4 +1,11 @@
+cd '/home/peter/quantlib/QuantLib/Examples'
+! N=25 DELPERC=0.80 ./laplaceinterpolation >laplaceinterpolation.dat
 set hidden3d
-splot 'laplaceinterpolation.dat' u 1:2:3 w l
-splot 'laplaceinterpolation.dat' u 1:2:4 w l
-splot 'laplaceinterpolation.dat' u 1:2:($3*1/(1.0-$5)) w p
+unset key
+splot 'laplaceinterpolation.dat' u 1:2:3:3 w p pt 7 ps 0.5 palette # original
+splot 'laplaceinterpolation.dat' u 1:2:($3*1/(1.0-$5)):3 w p pt 7 ps 0.5 palette # destructed
+splot 'laplaceinterpolation.dat' u 1:2:4:4 w p pt 7 ps 0.5 palette # reconstructued
+splot 'laplaceinterpolation.dat' u 1:2:($4-$3) w pm3d # error
+
+splot 'laplaceinterpolation.dat' u 1:2:3 w pm3d # original
+splot 'laplaceinterpolation.dat' u 1:2:4 w pm3d # reconstructued
