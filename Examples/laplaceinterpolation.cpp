b765256c381325560f105c1de8ef7eb9889f9557
Peter Caspers <pcaspers1973@gmail.com>
2015-11-21 15:42:52 +0100

    add mode flag

--- Examples/laplaceinterpolation.cpp
+++ Examples/laplaceinterpolation.cpp
@@ -8,12 +8,16 @@ int main() {
 
     int N = 100;
     double delPerc = 0.9;
+    int mode = 0;
     char *arg1 = getenv("DELPERC");
     if (arg1 != NULL)
         delPerc = atof(arg1);
     char *arg2 = getenv("N");
     if (arg2 != NULL)
         N = atoi(arg2);
+    char *arg3 = getenv("MODE");
+    if (arg3 != NULL)
+        mode = atoi(arg3);
 
     Matrix sample(N, N);
 
@@ -26,32 +30,39 @@ int main() {
 
     Matrix sample2(sample), del(N, N);
 
-    // delete random points
-    // MersenneTwisterUniformRng mt(42);
-    // for (Size l = 0; l < (N * N * delPerc); ++l) {
-    //     Size i = static_cast<Size>(mt.nextReal() * N);
-    //     Size j = static_cast<Size>(mt.nextReal() * N);
-    //     if (sample2[i][j] != Null<Real>()) {
-    //         sample2[i][j] = Null<Real>();
-    //         del[i][j] = 1;
-    //     } else {
-    //         --l;
-    //     }
-    // }
+    if (mode == 0) {
+        // delete random points
+        MersenneTwisterUniformRng mt(42);
+        for (Size l = 0; l < (N * N * delPerc); ++l) {
+            Size i = static_cast<Size>(mt.nextReal() * N);
+            Size j = static_cast<Size>(mt.nextReal() * N);
+            if (sample2[i][j] != Null<Real>()) {
+                sample2[i][j] = Null<Real>();
+                del[i][j] = 1;
+            } else {
+                --l;
+            }
+        }
+    }
 
-    // delete cols (starting at center)
-    // for(Size i=0;i<sample2.rows();++i) {
-    //     for(Size j= (N-delPerc*N)/2.0; j<=(N+delPerc*N)/2.0;++j) {
-    //         sample2[i][j] = Null<Real>();
-    //         del[i][j] = 1;
-    //     }
-    // }
+    if (mode == 1) {
+        // delete cols (starting at center)
+        for (Size i = 0; i < sample2.rows(); ++i) {
+            for (Size j = (N - delPerc * N) / 2.0; j <= (N + delPerc * N) / 2.0;
+                 ++j) {
+                sample2[i][j] = Null<Real>();
+                del[i][j] = 1;
+            }
+        }
+    }
 
-    // delete cols (on right side)
-    for (Size i = 0; i < sample2.rows(); ++i) {
-        for (Size j = (N - delPerc * N); j < N; ++j) {
-            sample2[i][j] = Null<Real>();
-            del[i][j] = 1;
+    if (mode == 2) {
+        // delete cols (on right side)
+        for (Size i = 0; i < sample2.rows(); ++i) {
+            for (Size j = (N - delPerc * N); j < N; ++j) {
+                sample2[i][j] = Null<Real>();
+                del[i][j] = 1;
+            }
         }
     }
 
