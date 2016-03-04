0a74405926f072caf52cefa7a9f7cf5487b7f46c
Peter Caspers <pcaspers1973@gmail.com>
2016-03-04 21:23:24 +0100

    and even more tidy up after merge

--- ql/models/shortrate/onefactormodels/markovfunctional.hpp
+++ ql/models/shortrate/onefactormodels/markovfunctional.hpp
@@ -400,12 +400,9 @@ namespace QuantLib {
         const Disposable<Array> zerobondArray(const Time T, const Time t,
                                               const Array &y) const;
 
-<<<<<<< HEAD
-=======
         Real deflatedZerobond(const Time T, const Time t = 0.0,
                               const Real y = 0.0) const;
 
->>>>>>> vendor
         // the following methods (tagged internal) are indended only to produce
         // the volatility diagnostics in the model outputs
         // due to the special convention of the instruments used for numeraire
