cdd3ab741ec640ed64b56225625f84b8bff95e9e
Peter Caspers <peter.caspers@quaternion.com>
2018-12-27 19:30:20 +0100

    format, add reference, polish a bit

--- ql/experimental/preexperimental/complexErf.cpp
+++ ql/experimental/preexperimental/complexErf.cpp
@@ -1,32 +1,33 @@
-#include <complexErf.hpp>
+#include "complexErf.hpp"
 
-using namespace std;
+#include <ql/math/comparison.hpp>
+#include <ql/math/distributions/normaldistribution.hpp>
+
+#include <boost/math/special_functions/erf.hpp>
 
 namespace QuantLib {
-
-	complex<double> erfc(complex<double> z) {
-		CumulativeNormalDistribution cnd;
-		double x=z.real();
-		double y=z.imag();
-		double erfr=2.0*cnd(sqrt(2.0)*x)-1.0;	double a=erfr;
-		double erfi=0.0;
-		double emxs=exp(-x*x);
-		if(x!=0.0) erfr+=emxs/(2.0*M_PI*x)*(1.0-cos(2.0*x*y));
-		if(x!=0.0) erfi+=emxs/(2.0*M_PI*x)*sin(2.0*x*y);
-			else erfi+=y/(M_PI);
-		double rr=0.0,ri=0.0,nd;
-		for(int n=1;n<=10;n++) {
-			nd=(double)n;
-			rr+=exp(-0.25*nd*nd)/(nd*nd+4.0*x*x)*(2.0*x-2.0*x*cosh(nd*y)*cos(2.0*x*y)+nd*sinh(n*y)*sin(2.0*x*y));
-			ri+=exp(-0.25*nd*nd)/(nd*nd+4.0*x*x)*(2.0*x*cosh(nd*y)*sin(2.0*x*y)+nd*sinh(nd*y)*cos(2.0*x*y));
-		}
-		rr*=2.0/M_PI*emxs;
-		ri*=2.0/M_PI*emxs;
-		/*FILE *out=fopen("erf.log","a");
-		fprintf(out,"%f;%f => %f;%f;%f\n",x,y,erfr+rr,erfi+ri,a);
-		fclose(out);*/
-		return complex<double>(erfr+rr,erfi+ri);
-	}
-
+std::complex<double> erf(const std::complex<double>& z, const std::size_t order) {
+    const double x = z.real();
+    const double y = z.imag();
+    double erfr = boost::math::erf(x);
+    double erfi = 0.0;
+    const double emxs = std::exp(-x * x);
+    if (!close_enough(x, 0.0)) {
+        erfr += emxs / (2.0 * M_PI * x) * (1.0 - cos(2.0 * x * y));
+        erfi += emxs / (2.0 * M_PI * x) * sin(2.0 * x * y);
+    } else {
+        erfi += y / (M_PI);
+    }
+    double rr = 0.0, ri = 0.0;
+    for (int n = 1; n <= order; ++n) {
+        double nd = static_cast<double>(n);
+        rr += exp(-0.25 * nd * nd) / (nd * nd + 4.0 * x * x) *
+              (2.0 * x - 2.0 * x * cosh(nd * y) * cos(2.0 * x * y) + nd * sinh(n * y) * sin(2.0 * x * y));
+        ri += exp(-0.25 * nd * nd) / (nd * nd + 4.0 * x * x) *
+              (2.0 * x * cosh(nd * y) * sin(2.0 * x * y) + nd * sinh(nd * y) * cos(2.0 * x * y));
+    }
+    rr *= 2.0 / M_PI * emxs;
+    ri *= 2.0 / M_PI * emxs;
+    return std::complex<double>(erfr + rr, erfi + ri);
 }
-
+} // namespace QuantLib
--- ql/experimental/preexperimental/complexErf.hpp
+++ ql/experimental/preexperimental/complexErf.hpp
@@ -1,22 +1,17 @@
 /*! \file complexErf.hpp
-\brief Some stand alone utility functinos
-Peter Caspers
+    \brief complex error function
 */
 
 #ifndef quantlib_complexErf_hpp
 #define quantlib_complexErf_hpp
 
-#include <ql/quantlib.hpp>
 #include <complex>
-#include <math.h>
-
-using namespace std;
 
 namespace QuantLib {
-
-	complex<double> erfc(complex<double> z);
-		
-
-}
+/* Reference: Benhamou, Eric and Croissant, Olivier, Local Time for the SABR Model:
+   Connection with the 'Complex' Black Scholes and Application to CMS and Spread Options (October 2007).
+   Available at SSRN: https://ssrn.com/abstract=1064461 */
+std::complex<double> erf(const std::complex<double>& z, const std::size_t order = 10);
+} // namespace QuantLib
 
 #endif
